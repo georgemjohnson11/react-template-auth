d21b119655dcd59f8b34301c9835336e9a065f09
George Johnson <georgemjohnson11@gmail.com>
2021-08-06 20:00:17 -0600

    Add Login and Signup buttons/forms and flush out NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Home from "./components/Home";
+import Login from './views/auth/Login';
+import Signup from './views/auth/Signup';
+import Logout from './views/auth/Logout';
+import Dashboard from './views/app/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
+        <Router>
+          <Header />
+          <Switch>
+            <Route path='/login' component={Login} exact />
+            <Route path='/signup' component={Signup} exact />
+            <Route path='/logout' component={Logout} exact />
+            <Route path='/dashboard' component={Dashboard} exact />
+          </Switch>
+        </Router>
         <Home />
         <Footer />
       </Fragment>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,16 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Title from './Title';
+import Navbar from "./NavBar";
 
 function                                                                   Header()
   {
     return (
         <div class="jumbotron text-center">
+        <Fragment>
         <Title />
-        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
+      </Fragment>
       </div>
     )
   }
